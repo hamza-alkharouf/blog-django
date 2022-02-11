6fa8307e548088eb6b81026e1850b3314bf11adf
hamza-alkharouf <hamzaalkharouf5@gmail.com>
2022-02-12 00:50:13 +0200

    add navbar

--- src/my_blog/settings.py
+++ src/my_blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,6 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# If you want to make Statis folder for all project you can use this code.
+# STATISFILES_DIRS = [
+#     os.path.join(BASE_DIR,"static"),
+#     r'C:\Users\hamza\Documents\GitHub\blog-django\src\static',
+# ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
