865ab90ded0fe94cbd25d60ecfc2203987517d3a
hamza-alkharouf <hamzaalkharouf5@gmail.com>
2022-02-12 21:01:06 +0200

    add page details and edit sidebar

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,32 @@
+from turtle import title
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    #CharField jast one line
+    title = models.CharField(max_length=100)
+    #TextField many line
+    content = models.TextField()
+
+    post_date = models.DateTimeField(default=timezone.now)
+    #auto_now each upate post 
+    post_update = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+    class Meta:
+        ordering =('-post_date', )
+ 
+
+class Comment(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    body = models.TextField()
+    comment_date = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,37 +1,10 @@
-from django.shortcuts import render
-
-
-posts = [
-    {
-        'title' :'التدوينى الاولى',
-        'content' :'نص التدوينة الاولى كنص تجريبي',
-        'post_date':'10-4-2019',
-        'author':'hamza alkh'
-    },
-    {
-        'title' :'التدوينى الثانية',
-        'content' :'نص التدوينة الثانية كنص تجريبي',
-        'post_date':'8-1-2020',
-        'author':'sami assaf'
-    },
-    {
-        'title' :'التدوينى الثالثة',
-        'content' :'نص التدوينة الثالثة كنص تجريبي',
-        'post_date':'25-3-2020',
-        'author':'ali ahmad'
-    },
-    {
-        'title' :'التدوينى الرابعة',
-        'content' :'نص التدوينة الرابعة كنص تجريبي',
-        'post_date':'2-12-2022',
-        'author':'rami alkh'
-    },
-]
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 def home(request):
     context ={
         'title' : 'الصفحة الرئيسية',
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/index.html', context)
 
@@ -40,3 +13,11 @@ def about(request):
         'title' : 'من انا',
     }
         return render(request, 'blog/about.html', context)
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = { 
+        'title': post,
+        'post' : post,
+    }
+    return render(request, 'blog/detail.html', context)
