2d580055f467e19da318e5582eb8b3984612583c
hamza-alkharouf <hamzaalkharouf5@gmail.com>
2022-02-12 03:09:43 +0200

    create about page and andd content in home

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,42 @@
 from django.shortcuts import render
 
+
+posts = [
+    {
+        'title' :'التدوينى الاولى',
+        'content' :'نص التدوينة الاولى كنص تجريبي',
+        'post_date':'10-4-2019',
+        'author':'hamza alkh'
+    },
+    {
+        'title' :'التدوينى الثانية',
+        'content' :'نص التدوينة الثانية كنص تجريبي',
+        'post_date':'8-1-2020',
+        'author':'sami assaf'
+    },
+    {
+        'title' :'التدوينى الثالثة',
+        'content' :'نص التدوينة الثالثة كنص تجريبي',
+        'post_date':'25-3-2020',
+        'author':'ali ahmad'
+    },
+    {
+        'title' :'التدوينى الرابعة',
+        'content' :'نص التدوينة الرابعة كنص تجريبي',
+        'post_date':'2-12-2022',
+        'author':'rami alkh'
+    },
+]
+
 def home(request):
-    return render(request, 'blog/index.html', {'title': 'Home'})
+    context ={
+        'title' : 'الصفحة الرئيسية',
+        'posts' : posts
+    }
+    return render(request, 'blog/index.html', context)
+
+def about(request):
+        context ={
+        'title' : 'من انا',
+    }
+        return render(request, 'blog/about.html', context)
