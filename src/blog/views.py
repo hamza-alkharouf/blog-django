83ce44cc20f83f4d99d8d4c55786a8f4594af884
hamza-alkharouf <hamzaalkharouf5@gmail.com>
2022-02-16 21:47:25 +0200

    save data

--- src/blog/views.py
+++ src/blog/views.py
@@ -18,14 +18,29 @@ def about(request):
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
 
-    comment_form = NewComment()
 
     #comments is rekated name of model class Comment
     comments = post.comments.filter(active=True)
+
+    
+    #check before save data from comment form
+    if request.method == 'POST':
+        comment_form = NewComment(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            new_comment = NewComment()
+    else:
+        comment_form = NewComment()
+        
+
     context = { 
         'title': post,
         'post' : post,
         'comments' :comments,
         'comment_form' :comment_form,
     }
+
+    
     return render(request, 'blog/detail.html', context)
